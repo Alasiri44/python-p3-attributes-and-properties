785c8d6e05e2ed8e4841fd5fa1550fad9962d3c1
alasiri <austinalasiri44@gmail.com>
2025-05-11 18:36:00 +0300

    My commits

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import re
 
 APPROVED_BREEDS = [
     "Mastiff",
@@ -12,4 +13,31 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self,breed=APPROVED_BREEDS[0], name='fido',):
+        self.name = name
+        self.breed = breed
+
+    def get_name(self):
+        print('Getting the name')       
+        return self._name
+        
+    def set_name(self, name):
+        # print(f'Setting the name equal to {name}')
+        if(type(name) != str) or (len(name) > 25) or (len(name) < 1) or (not re.search('[a-zA-Z]',name)):
+            print("Name must be string between 1 and 25 characters.")        
+        elif(0 < len(name) < 25):
+            self._name = name
+            
+    def get_breed(self):
+        print('Getting the breed')       
+        return self._breed
+
+    def set_breed(self, breed):
+        if(breed not in APPROVED_BREEDS):  
+            print("Breed must be in list of approved breeds.")
+        else:
+            self._breed = breed
+            
+    name = property(get_name, set_name)
+    breed = property(get_breed, set_breed)
+    
--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import re
 
 APPROVED_JOBS = [
     "Admin",
@@ -16,4 +17,31 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self,job=APPROVED_JOBS[0], name='Guido',):
+        self.name = name
+        self.job = job
+
+    def get_name(self):
+        print('Getting the name')       
+        return self._name
+        
+    def set_name(self, name):
+        # print(f'Setting the name equal to {name}')
+        if(type(name) != str) or (len(name) > 25) or (len(name) < 1) or (not re.search('[a-zA-Z]',name)):
+            print("Name must be string between 1 and 25 characters.")        
+        elif(0 < len(name) < 25):
+            self._name = name.title()
+            
+    def get_job(self):
+        print('Getting the job')       
+        return self._job
+
+    def set_job(self, job):
+        if(job not in APPROVED_JOBS):  
+            print("Job must be in list of approved jobs.")
+        else:
+            self._job = job
+            
+    name = property(get_name, set_name)
+    job = property(get_job, set_job)
+    
